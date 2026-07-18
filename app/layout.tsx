31a0b844a4bbabf07e8de1849cae010e32d212fb
Your Name <you@example.com>
2026-07-18 09:22:15 +0800

    fixed hourly on TL

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
